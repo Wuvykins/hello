918e169b1ecbe3aae6a2a9e87342aba668e05f74
Nic Alexander <wuvykins@gmail.com>
2019-01-07 14:30:54 -0500

    add index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors' , 1);
+error_reporting(E_ALL);
+
+//Require
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Hello, World</h1>';
+});
+
+//Run fat free
+$f3->run();
